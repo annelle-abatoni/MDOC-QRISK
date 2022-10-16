31b5c4bfdd1c01e008da0b51ecb1d2cc3e66c44d
Esther Nanjala Wekesa <55264138+Nwekesa@users.noreply.github.com>
2022-10-16 17:59:06 -0400

    Update register.php

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:qriskcalcmdoc.database.windows.net,1433; Database = QriskCalculator", "qriskadmin", "{Calculator1!}");
+    $conn = new PDO("sqlsrv:server = tcp:qriskcalcmdoc.database.windows.net,1433; Database = QriskCalculator", "qriskadmin", "{password}");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -10,7 +10,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "qriskadmin", "pwd" => "{Calculator1!}", "Database" => "QriskCalculator", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "qriskadmin", "pwd" => "{password}", "Database" => "QriskCalculator", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:qriskcalcmdoc.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
